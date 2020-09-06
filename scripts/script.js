bb7a813dc6988b2ebd6d1ac0f41306b08e6e7872
JADEN <29948746+JADENgygo@users.noreply.github.com>
2020-09-06 18:00:05 +0900

    ボタンクリック後の通知を実装

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,18 @@ window.addEventListener('DOMContentLoaded', () => {
 		e.addEventListener('click', () => {
 			const dummy = document.createElement('div');
 			dummy.textContent = 'https://jadengygo.github.io/princess-connect-stickers/img/' + e.id + '.png';
-			console.log(dummy.textContent);
 			dummy.style.visible = 'hidden'
 			document.body.appendChild(dummy);
 			document.getSelection().selectAllChildren(dummy);
 			document.execCommand('copy');
 			document.body.removeChild(dummy);
+
+			UIkit.notification.closeAll();
+			UIkit.notification({
+				message: '画像リンクをコピーしました',
+				pos: 'top-center',
+				timeout: 500
+			});
 		});
 	});
 });
