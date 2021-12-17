4630e7458371ae116d4486e1ab7f26c68366d978
JADEN <29948746+JADENgygo@users.noreply.github.com>
2021-12-18 01:15:33 +0900

    impl test

--- pages/alone.tsx
+++ pages/alone.tsx
@@ -1,23 +1,24 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
 
+export const stickers = [
+  { name: 'どうしてこんなことに…?', path: '/img/alone/day1.webp', width: 800, height: 680 },
+  { name: 'ごきげんようが言えなくて', path: '/img/alone/day2.webp', width: 800, height: 680 },
+  { name: 'やっちゃった!', path: '/img/alone/day3.webp', width: 800, height: 680 },
+  { name: 'ユニさんはこんな人', path: '/img/alone/day4.webp', width: 800, height: 680 },
+  { name: 'クロエさんはこんな人', path: '/img/alone/day5.webp', width: 800, height: 680 },
+  { name: 'チエルさんはこんな人', path: '/img/alone/day6.webp', width: 800, height: 680 },
+  { name: '知的なユニさん', path: '/img/alone/day7.webp', width: 800, height: 680 },
+  { name: '優しいクエロさん', path: '/img/alone/day8.webp', width: 800, height: 680 },
+  { name: '憧れのチエルさん', path: '/img/alone/day9.webp', width: 800, height: 680 },
+  { name: '特別講座の練習', path: '/img/alone/day10.webp', width: 800, height: 680 },
+  { name: 'いよいよ特別講座', path: '/img/alone/day11.webp', width: 800, height: 680 },
+  { name: 'さすがBB団の団長さん!', path: '/img/alone/day12.webp', width: 800, height: 680 },
+  { name: 'マンドラゴラでパニック', path: '/img/alone/day13.webp', width: 800, height: 680 },
+  { name: '最終日', path: '/img/alone/day14.webp', width: 800, height: 680 },
+];
+
 const Alone: NextPage = () => {
-  const stickers = [
-    { name: 'どうしてこんなことに…?', path: '/img/alone/day1.webp', width: 800, height: 680 },
-    { name: 'ごきげんようが言えなくて', path: '/img/alone/day2.webp', width: 800, height: 680 },
-    { name: 'やっちゃった!', path: '/img/alone/day3.webp', width: 800, height: 680 },
-    { name: 'ユニさんはこんな人', path: '/img/alone/day4.webp', width: 800, height: 680 },
-    { name: 'クロエさんはこんな人', path: '/img/alone/day5.webp', width: 800, height: 680 },
-    { name: 'チエルさんはこんな人', path: '/img/alone/day6.webp', width: 800, height: 680 },
-    { name: '知的なユニさん', path: '/img/alone/day7.webp', width: 800, height: 680 },
-    { name: '優しいクエロさん', path: '/img/alone/day8.webp', width: 800, height: 680 },
-    { name: '憧れのチエルさん', path: '/img/alone/day9.webp', width: 800, height: 680 },
-    { name: '特別講座の練習', path: '/img/alone/day10.webp', width: 800, height: 680 },
-    { name: 'いよいよ特別講座', path: '/img/alone/day11.webp', width: 800, height: 680 },
-    { name: 'さすがBB団の団長さん!', path: '/img/alone/day12.webp', width: 800, height: 680 },
-    { name: 'マンドラゴラでパニック', path: '/img/alone/day13.webp', width: 800, height: 680 },
-    { name: '最終日', path: '/img/alone/day14.webp', width: 800, height: 680 },
-  ];
   return (
     <div className="container">
       <div className="row gy-3 row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6 text-center">
--- pages/angel.tsx
+++ pages/angel.tsx
@@ -1,20 +1,21 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
 
+export const stickers = [
+  { name: '次世代へ継承すべき新言語', path: '/img/angel/chieru.webp', width: 800, height: 680 },
+  { name: '私はとても作文が苦手です', path: '/img/angel/kuroe.webp', width: 800, height: 680 },
+  { name: '飛翔へと至るまで', path: '/img/angel/yuni.webp', width: 800, height: 680 },
+  { name: '閃きましたわ!', path: '/img/angel/akino.webp', width: 800, height: 680 },
+  { name: '組み立てにゃ!', path: '/img/angel/tamaki.webp', width: 800, height: 680 },
+  { name: '私こんなの書いたっけ…?', path: '/img/angel/yukari.webp', width: 800, height: 680 },
+  { name: 'このままにしておけないわ', path: '/img/angel/mihuyu.webp', width: 800, height: 680 },
+  { name: '特別講座を終えて', path: '/img/angel/mercurius.webp', width: 800, height: 680 },
+  { name: '考察', path: '/img/angel/nakayoshix1.webp', width: 800, height: 680 },
+  { name: '試作機完成', path: '/img/angel/nakayoshix2.webp', width: 800, height: 680 },
+  { name: '最終工程', path: '/img/angel/nakayoshix3.webp', width: 800, height: 680 },
+];
+
 const Angel: NextPage = () => {
-  const stickers = [
-    { name: '次世代へ継承すべき新言語', path: '/img/angel/chieru.webp', width: 800, height: 680 },
-    { name: '私はとても作文が苦手です', path: '/img/angel/kuroe.webp', width: 800, height: 680 },
-    { name: '飛翔へと至るまで', path: '/img/angel/yuni.webp', width: 800, height: 680 },
-    { name: '閃きましたわ!', path: '/img/angel/akino.webp', width: 800, height: 680 },
-    { name: '組み立てにゃ!', path: '/img/angel/tamaki.webp', width: 800, height: 680 },
-    { name: '私こんなの書いたっけ…?', path: '/img/angel/yukari.webp', width: 800, height: 680 },
-    { name: 'このままにしておけないわ', path: '/img/angel/mihuyu.webp', width: 800, height: 680 },
-    { name: '特別講座を終えて', path: '/img/angel/mercurius.webp', width: 800, height: 680 },
-    { name: '考察', path: '/img/angel/nakayoshix1.webp', width: 800, height: 680 },
-    { name: '試作機完成', path: '/img/angel/nakayoshix2.webp', width: 800, height: 680 },
-    { name: '最終工程', path: '/img/angel/nakayoshix3.webp', width: 800, height: 680 },
-  ];
   return (
     <div className="container">
       <div className="row gy-3 row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6 text-center">
