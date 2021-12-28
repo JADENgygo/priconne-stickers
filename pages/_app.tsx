424ae579eee1ff4e98170c4eba8fb00b330edf56
JADEN <29948746+JADENgygo@users.noreply.github.com>
2021-12-28 21:10:43 +0900

    rename

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import type { AppProps } from 'next/app'
-import { HeadTag } from '../components/headTag'
+import { Head_ } from '../components/head_'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="content">
-      <HeadTag />
+      <Head_ />
       <Header />
       <Component {...pageProps} />
       <Footer className="footer" />
